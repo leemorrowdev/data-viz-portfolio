7c41da0e888215c8e27891819ec826dcab34ce36
leemorrowdev <leemorrow.dev@gmail.com>
2020-05-20 12:08:41 -0400

    Update font and colors

--- src/components/layout/main/index.js
+++ src/components/layout/main/index.js
@@ -36,7 +36,7 @@ const MainLayout = ({ children }) => {
     <>
       <Helmet>
         <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&display=swap"
           rel="stylesheet"
         />
       </Helmet>
