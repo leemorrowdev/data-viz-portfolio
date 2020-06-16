02c35b0ab42ac4a292ad25b3bcc7e36c616329ba
leemorrowdev <leemorrow.dev@gmail.com>
2020-06-16 11:21:25 -0400

    Self-host fonts

--- src/components/layout/main/index.js
+++ src/components/layout/main/index.js
@@ -4,7 +4,6 @@
 
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { Helmet } from "react-helmet"
 
 import Header from "../header"
 import Footer from "../footer"
@@ -34,12 +33,6 @@ const MainLayout = ({ children }) => {
 
   return (
     <>
-      <Helmet>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
       <div className={styles.container}>
         <Header siteTitle={title} siteSocial={social} />
         <main>{children}</main>
