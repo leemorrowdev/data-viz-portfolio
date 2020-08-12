23948e0d1cbfe0e9273593308b76b5aaf8157c9f
Lee Morrow <leemorrow.dev@gmail.com>
2020-08-12 15:27:04 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-50a00cd1aa6d149b2662.js"
+    "url": "webpack-runtime-03c0158f49b5d5a63424.js"
   },
   {
     "url": "styles.b37bf8e7f0ecc1819548.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "10c40cf2967e43b28ef0a23d778fdaa9"
+    "revision": "b379a57c73527b113b15eb224c8ff1e0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-18b9bd78e89bbe4752a5.js"
