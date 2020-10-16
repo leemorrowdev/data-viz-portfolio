9dc39037d1fd5749048bf72579806b173b3c8afe
Lee Morrow <leemorrow.dev@gmail.com>
2020-10-16 14:36:43 -0400

    Updates

--- component---src-pages-404-js-7236591f5e059bec2620.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),l=n("5uII"),r=n("vrFN");t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(r.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist. This is very sad."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-7236591f5e059bec2620.js.map
