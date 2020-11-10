d1bb0939a452bcf4636a17350f21d3f911a3c958
Lee Morrow <leemorrow.dev@gmail.com>
2020-11-10 11:47:04 -0500

    Responsive SVGs base

---
+++ src/content/projects/project-one/chart/index.js
@@ -0,0 +1,27 @@
+/**
+ * Test visualization
+ */
+
+import React, { useEffect, useRef } from "react"
+import * as d3 from "d3"
+
+import { useChartDimensions } from "../../../../modules/hooks"
+import styles from "./chart.module.scss"
+
+export default function Chart({ settings }) {
+  const [wrapperRef, dms] = useChartDimensions(settings)
+  const svgRef = useRef();
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+    
+  }, [dms]);
+
+  return (
+    <div className={styles.container} ref={wrapperRef}>
+      <svg className={styles.chart} ref={svgRef} width={dms.width} height={dms.height}>
+
+      </svg>
+    </div>
+  )
+}
